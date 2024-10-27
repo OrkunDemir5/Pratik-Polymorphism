20361362f228fad2793500aa517e364336e682d2
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-28 00:10:28 +0300

    Pratik-Polymorphism

---
+++ Pratik - Polymorphism/Pratik - Polymorphism/GeometrikSekil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Polymorphism
+{
+    class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        // AlanHesapla metodu Base class içinde tanımlanır ve polimorfizm sağlanır.
+        public virtual double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+    class Kare : BaseGeometrikSekil
+    {
+        // Kare hesaplama
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+    class Dikdortgen : BaseGeometrikSekil
+    {
+        // Dikdörtgen hesaplama
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+    class DikUcgen : BaseGeometrikSekil
+    {
+        // Dik Üçgende hesaplama
+        public override double AlanHesapla()
+        {
+            return (Genislik * Yukseklik) / 2;
+        }
+    }
+}
---
+++ Pratik - Polymorphism/Pratik - Polymorphism/Program.cs
@@ -0,0 +1,13 @@
+﻿using Pratik_Polymorphism;
+
+// Kare için nesne oluşturulup alan hesaplanır
+Kare kare = new Kare { Genislik = 5, Yukseklik = 5 };
+Console.WriteLine($"Kare Alanı: {kare.AlanHesapla()}");
+
+// Dikdörtgen için nesne oluşturulup alan hesaplama
+Dikdortgen dikdortgen = new Dikdortgen { Genislik = 4, Yukseklik = 6 };
+Console.WriteLine($"Dikdörtgen Alanı: {dikdortgen.AlanHesapla()}");
+
+// Dik Üçgen için nesne oluşturulup alan hesaplama
+DikUcgen dikUcgen = new DikUcgen { Genislik = 3, Yukseklik = 7 };
+Console.WriteLine($"Dik Üçgen Alanı: {dikUcgen.AlanHesapla()}");
